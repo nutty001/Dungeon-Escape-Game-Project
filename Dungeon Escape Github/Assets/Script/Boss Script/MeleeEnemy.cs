f4a3be914a0dd83e2a6e185b75e88054d2f28343
nutty001 <pratikthapa328@gmail.com>
2022-07-10 19:36:06 +0530

    Main boss attack
    Main boss can new attack when player is within damage range.

--- Dungeon Escape Github/Assets/Script/Boss Script/MeleeEnemy.cs
+++ Dungeon Escape Github/Assets/Script/Boss Script/MeleeEnemy.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
+    [Header ("Attack Parameters")]
     [SerializeField] private float attackCooldown;
-    [SerializeField] private float range;
-    [SerializeField] private float colliderDistance;
+    [SerializeField] private float range; 
     [SerializeField] private int damage;
+
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
     [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
 
     private Animator anim;
+    private Health playerHealth;
+    private EnemyPatrol enemyPatrol;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
 
     private void Update()
@@ -32,6 +40,8 @@ public class MeleeEnemy : MonoBehaviour
             }
         }
 
+        if (enemyPatrol != null)
+            enemyPatrol.enabled = !PlayerInSight();
     }
 
     private bool PlayerInSight()
@@ -39,6 +49,10 @@ public class MeleeEnemy : MonoBehaviour
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x*range,boxCollider.bounds.size.y,boxCollider.bounds.size.y),
             0,Vector2.left,0,playerLayer);
+
+        if (hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+
         return hit.collider!=null;
 
     }
@@ -49,4 +63,6 @@ public class MeleeEnemy : MonoBehaviour
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.y));
     }
+
+   
 }
